6b39b97bd1f0848142dc12063625a574c2458cf0
root <jx-sec@outlook.com>
2022-11-21 17:17:19 +0800

    update

--- server/report_raw_log_ch.py
+++ server/report_raw_log_ch.py
@@ -67,7 +67,7 @@ def ch_report_get_raw_full_log(request):
         client = Client(host=sys_report_conf_result.ch_host, port=int(sys_report_conf_result.ch_port),
                         user=sys_report_conf_result.ch_user,
                         password=sys_report_conf_result.ch_password, database=sys_report_conf_result.ch_database)
-        start_sql_query = 'select * from jxwaf.jxlog'
+        start_sql_query = 'select BytesReceived,BytesSent,ConnectionsActive,ConnectionsWaiting,ContentLength,ContentType,Cookie,Host,Method,ProcessTime,QueryString,RawBody,RawHeaders,UserAgent,Accept,AcceptEncoding,Origin,Referer,UpgradeInsecureRequests,AcceptLanguage,RawRespHeadersconnection,RawRespHeaderscontentEncoding,RawRespHeaderscontentType,RawRespHeaderstransferEncoding,RequestID,RequestTime,Scheme,SrcIP,SslCiphers,SslProtocol,Status,UpstreamAddr,UpstreamBytesReceived,UpstreamBytesSent,UpstreamResponseTime,UpstreamStatus,URI,Version,WafAction,WafExtra,WafModule,WafNodeUUID,WafPolicy,XForwardedFor from jxwaf.jxlog'
         end_sql_query = "RequestTime > '{}' and RequestTime < '{}' limit {},{} ".format(start_time, end_time,
                                                                                         limit_start, limit_end)
         rules = json.loads(sql_query_rule)
@@ -151,7 +151,7 @@ def ch_report_custom_get_raw_full_log(request):
         client = Client(host=sys_report_conf_result.ch_host, port=int(sys_report_conf_result.ch_port),
                         user=sys_report_conf_result.ch_user,
                         password=sys_report_conf_result.ch_password, database=sys_report_conf_result.ch_database)
-        start_sql_query = 'select * from jxwaf.jxlog where '
+        start_sql_query = 'select BytesReceived,BytesSent,ConnectionsActive,ConnectionsWaiting,ContentLength,ContentType,Cookie,Host,Method,ProcessTime,QueryString,RawBody,RawHeaders,UserAgent,Accept,AcceptEncoding,Origin,Referer,UpgradeInsecureRequests,AcceptLanguage,RawRespHeadersconnection,RawRespHeaderscontentEncoding,RawRespHeaderscontentType,RawRespHeaderstransferEncoding,RequestID,RequestTime,Scheme,SrcIP,SslCiphers,SslProtocol,Status,UpstreamAddr,UpstreamBytesReceived,UpstreamBytesSent,UpstreamResponseTime,UpstreamStatus,URI,Version,WafAction,WafExtra,WafModule,WafNodeUUID,WafPolicy,XForwardedFor from jxwaf.jxlog where '
         end_sql_query = "RequestTime > '{}' and RequestTime < '{}' limit {},{} ".format(start_time, end_time,
                                                                                         limit_start, limit_end)
         sql_query = start_sql_query + custom_sql_query + end_sql_query
