aafdee0ba3b8f11257ef2ea31144bf4a0691f3d5
root <jx-sec@outlook.com>
2022-11-23 14:41:22 +0800

    update

--- server/report_raw_log_ch.py
+++ server/report_raw_log_ch.py
@@ -21,7 +21,7 @@ def ch_report_get_raw_log(request):
                         user=sys_report_conf_result.ch_user,
                         password=sys_report_conf_result.ch_password, database=sys_report_conf_result.ch_database)
         start_sql_query = 'select RequestTime,SrcIP,Method,Host,URI,UserAgent,Status,WafModule,WafPolicy,WafAction,RequestID from jxwaf.jxlog'
-        end_sql_query = "RequestTime > '{}' and RequestTime < '{}' limit {},{} ".format(start_time, end_time,
+        end_sql_query = "RequestTime > '{}' and RequestTime < '{}' order by RequestTime desc limit {},{} ".format(start_time, end_time,
                                                                                         limit_start, limit_end)
         rules = json.loads(sql_query_rule)
         rule_sql_query = ' where '
@@ -81,7 +81,7 @@ def ch_report_get_raw_full_log(request):
                         password=sys_report_conf_result.ch_password, database=sys_report_conf_result.ch_database)
         #start_sql_query = 'select BytesReceived,BytesSent,ConnectionsActive,ConnectionsWaiting,ContentLength,ContentType,Cookie,Host,Method,ProcessTime,QueryString,RawBody,RawHeaders,UserAgent,Accept,AcceptEncoding,Origin,Referer,UpgradeInsecureRequests,AcceptLanguage,RawRespHeadersconnection,RawRespHeaderscontentEncoding,RawRespHeaderscontentType,RawRespHeaderstransferEncoding,RequestID,RequestTime,Scheme,SrcIP,SslCiphers,SslProtocol,Status,UpstreamAddr,UpstreamBytesReceived,UpstreamBytesSent,UpstreamResponseTime,UpstreamStatus,URI,Version,WafAction,WafExtra,WafModule,WafNodeUUID,WafPolicy,XForwardedFor from jxwaf.jxlog'
         start_sql_query = 'select * from jxwaf.jxlog'
-        end_sql_query = "RequestTime > '{}' and RequestTime < '{}' limit {},{} ".format(start_time, end_time,
+        end_sql_query = "RequestTime > '{}' and RequestTime < '{}' order by RequestTime desc limit {},{} ".format(start_time, end_time,
                                                                                         limit_start, limit_end)
         rules = json.loads(sql_query_rule)
         rule_sql_query = ' where '
@@ -140,7 +140,7 @@ def ch_report_custom_get_raw_log(request):
                         user=sys_report_conf_result.ch_user,
                         password=sys_report_conf_result.ch_password, database=sys_report_conf_result.ch_database)
         start_sql_query = 'select RequestTime,SrcIP,Method,Host,URI,UserAgent,Status,WafModule,WafPolicy,WafAction,RequestID from jxwaf.jxlog where '
-        end_sql_query = " and RequestTime > '{}' and RequestTime < '{}' limit {},{} ".format(start_time, end_time,
+        end_sql_query = " and RequestTime > '{}' and RequestTime < '{}' order by RequestTime desc limit {},{} ".format(start_time, end_time,
                                                                                              limit_start, limit_end)
         sql_query = start_sql_query + custom_sql_query + end_sql_query
         try:
@@ -190,7 +190,7 @@ def ch_report_custom_get_raw_full_log(request):
                         password=sys_report_conf_result.ch_password, database=sys_report_conf_result.ch_database)
         # start_sql_query = 'select BytesReceived,BytesSent,ConnectionsActive,ConnectionsWaiting,ContentLength,ContentType,Cookie,Host,Method,ProcessTime,QueryString,RawBody,RawHeaders,UserAgent,Accept,AcceptEncoding,Origin,Referer,UpgradeInsecureRequests,AcceptLanguage,RawRespHeadersconnection,RawRespHeaderscontentEncoding,RawRespHeaderscontentType,RawRespHeaderstransferEncoding,RequestID,RequestTime,Scheme,SrcIP,SslCiphers,SslProtocol,Status,UpstreamAddr,UpstreamBytesReceived,UpstreamBytesSent,UpstreamResponseTime,UpstreamStatus,URI,Version,WafAction,WafExtra,WafModule,WafNodeUUID,WafPolicy,XForwardedFor from jxwaf.jxlog where '
         start_sql_query = 'select * from jxwaf.jxlog  where '
-        end_sql_query = " and RequestTime > '{}' and RequestTime < '{}' limit {},{} ".format(start_time, end_time,
+        end_sql_query = " and RequestTime > '{}' and RequestTime < '{}' order by RequestTime desc limit {},{} ".format(start_time, end_time,
                                                                                              limit_start, limit_end)
         sql_query = start_sql_query + custom_sql_query + end_sql_query
         try:
