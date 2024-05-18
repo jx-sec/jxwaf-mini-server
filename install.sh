94d6c1d0cf65650b545abaf3888e4648daab07cb
root <jx-sec@outlook.com>
2024-05-19 00:25:44 +0800

    u

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-yum install -y epel-release gcc make mysql mysql-devel python-devel
+yum install -y epel-release
 yum install -y python-pip
-pip install setuptools==30.1.0
 pip install -r requirements.txt
